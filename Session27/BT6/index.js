ca1fa3240d02daa054e87e51303062e447a8d611
SiwaAphm <siwaaphm@gmail.com>
2025-04-02 23:04:32 +0700

    update bt6

--- Session27/BT6/index.js
+++ Session27/BT6/index.js
@@ -1,24 +1,17 @@
-function isArithmeticSequence(arr) {
-  if (!Array.isArray(arr) || arr.length < 2 || !arr.every(Number.isInteger)) {
-    return "dữ liệu không hợp lệ";
+function check(arr) {
+  if (!Array.isArray(arr)) {
+    return "Dữ liệu ko hợp lệ";
   }
 
-  let diff = arr[1] - arr[0];
+  let num = arr[1] - arr[0];
 
-  for (let i = 1; i < arr.length - 1; i++) {
-    if (arr[i + 1] - arr[i] !== diff) {
-      return false;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] !== num) {
+      return "FALSE";
     }
   }
-  return true;
+  return "TRUE";
 }
 
-const testCases = [
-  [2, 4, 6, 8], // TRUE
-  [3, 6, 9, 12, 14], // FALSE
-  "abc" // Dữ liệu không hợp lệ
-];
-
-testCases.forEach(test => {
-  console.log(`Input: ${JSON.stringify(test)} -> Output:`, isArithmeticSequence(test));
-});
+let arr = [[2, 4, 6, 8]];
+alert(check(arr));
